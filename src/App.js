3f78e57d1c511ca7976f73ed3419c9de050b946f
Kok Kok <REMY@Koks-MacBook-Pro.local>
2020-08-21 11:37:07 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Heading } from './components/Heading';
+import {Balance } from './components/Balance';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Heading />
+      <div className="container">
+        <Balance/>
+      </div>
     </div>
   );
 }
